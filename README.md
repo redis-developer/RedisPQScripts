210920e75f0c980a1c2a4523f7b18c7ebb30e443
TusharRakheja <tusharrakheja@gmail.com>
2021-05-16 15:57:15 -0700

    Move to JSON module

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Redis Priority Queue Scripts &nbsp;[![Build Status](https://travis-ci.com/TusharRakheja/RedisPQScripts.svg?branch=master)](https://travis-ci.com/github/TusharRakheja/RedisPQScripts)
 
-These scripts are cached and executed server-side, and maintain the priority queue. 
+These scripts are cached and executed server-side, and maintain the priority queue.
 
 Made for #RedisConf 2021 Hackathon.
--- remove.lua
+++ remove.lua
@@ -1,7 +1,19 @@
 local app = KEYS[1]
-local list = app .. ':list'
-local counter = app .. ':counter'
+local list = 'list'
+local counter = 'counter'
 local is_peek = tonumber(KEYS[2])
+
+local dollarize = function (num)
+        return '$' .. tostring(num)
+end
+
+local list_at = function(index)
+        return list .. '[' .. index .. ']'
+end
+
+local counter_at = function(key)
+        return counter .. '.' .. key
+end
 
 local sift_down = function (index, heap_size)
         local keep_sifting
@@ -14,23 +26,23 @@ local sift_down = function (index, heap_size)
                                 return
                         else
                                 max_index = left_child_index
-                                max_data = tonumber(redis.call('HGET', list, left_child_index))
+                                max_data = tonumber(redis.call('JSON.GET', app, list_at(left_child_index)))
                         end
                 else
-                        local left_child = tonumber(redis.call('HGET', list, left_child_index))
-                        local right_child = tonumber(redis.call('HGET', list, right_child_index))
+                        local left_child = tonumber(redis.call('JSON.GET', app, list_at(left_child_index)))
+                        local right_child = tonumber(redis.call('JSON.GET', app, list_at(right_child_index)))
                         if (left_child > right_child) then
                                 max_index = left_child_index
-                                max_data = tonumber(redis.call('HGET', list, left_child_index))
+                                max_data = tonumber(redis.call('JSON.GET', app, list_at(left_child_index)))
                         else
                                 max_index = right_child_index
-                                max_data = tonumber(redis.call('HGET', list, right_child_index))
+                                max_data = tonumber(redis.call('JSON.GET', app, list_at(right_child_index)))
                         end
                 end
-                local current_val = tonumber(redis.call('HGET', list, index))
+                local current_val = tonumber(redis.call('JSON.GET', app, list_at(index)))
                 if (current_val < max_data) then
-                        redis.call('HSET', list, index, max_data)
-                        redis.call('HSET', list, max_index, current_val)
+                        redis.call('JSON.SET', app, list_at(index), max_data)
+                        redis.call('JSON.SET', app, list_at(max_index), current_val)
                         index = max_index
                         keep_sifting = true
                 else
@@ -39,8 +51,8 @@ local sift_down = function (index, heap_size)
         until (keep_sifting)
 end
 
-local heap_size = tonumber(redis.call('HLEN', list))
-local max_priority = redis.call('HGET', list, 1)
+local heap_size = tonumber(redis.call('JSON.ARRLEN', app, list)) - 1
+local max_priority = redis.call('JSON.MGET', app, list_at(1))[1]
 
 if (heap_size == 0) then
         return nil
@@ -48,14 +60,15 @@ else
         if (is_peek == 1) then
                 do end
         else
-                local existing_count = tonumber(redis.call('HGET', counter, max_priority))
+                local dollar_max_priority = dollarize(max_priority)
+                local existing_count = tonumber(redis.call('JSON.MGET', app, counter_at(dollar_max_priority))[1])
                 if (existing_count == 1) then
-                        redis.call('HDEL', counter, max_priority)
-                        redis.call('HSET', list, 1, tonumber(redis.call('HGET', list, heap_size)))
-                        redis.call('HDEL', list, heap_size)
+                        redis.call('JSON.DEL', app, counter_at(dollar_max_priority))
+                        redis.call('JSON.SET', app, list_at(1), tonumber(redis.call('JSON.GET', app, list_at(heap_size))))
+                        redis.call('JSON.DEL', app, list_at(heap_size))
                         sift_down(1, heap_size - 1)
                 else
-                        redis.call('HSET', counter, max_priority, existing_count - 1)
+                        redis.call('JSON.SET', app, counter_at(dollar_max_priority), existing_count - 1)
                 end
         end
         return max_priority
